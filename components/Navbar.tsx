9d356a39f4b1d3bf68168de0fffcfa1a0c479c62
harsha-fullyintegratedgroup <harsha@fullyintegratedgroup.com>
2026-04-12 18:23:22 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
         <Link to="/">
           <img
-            src="/logo/mainlogo.png"
+            src="logo/mainlogo.png"
             alt="Fully Integrated Group"
           />
         </Link>
